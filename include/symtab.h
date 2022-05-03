25d8607395709c1468e83029f11f12853c07d8bd
Eduardo Assis <31329807+edassis@users.noreply.github.com>
2022-05-03 15:57:23 -0300

    Implements semantic analysis, code generation (#4)
    ## Code generation
    - Algebraic, relational, negation ops
    - Selection, iteration statements
    - Iteration stmts
    - Assignments
    - In/Out (ler, escrever)
    
    ## Syntax analyzer
    - Fixes if/else ambiguity, return ler();
    
    Co-authored-by: Oscar2019 <omadureira2000@gmail.com>

---
+++ include/symtab.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <map>
+#include <vector>
+
+enum SymbId{
+    NONE,
+    FN,
+    VAR,
+};
+
+
+struct Symb{
+    char* name;
+    int32_t size;
+    int32_t loc;
+    int32_t usado;
+    int type;
+    struct Symb *next;
+    int32_t nivel;
+};
+
+void append_new(char* name, int location, int type, int nivel, Symb** head);
+Symb* find(char* name, Symb** head);
+void used(char* name, Symb** head);
+void printTS(Symb** head);
+Symb** cria_lista();
